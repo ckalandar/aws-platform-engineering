35f32e5a7bcba23325b7a218b611037398fde1ba
ckalandar <ckalandar@gmail.com>
2026-07-12 17:17:43 +0530

    added oidc setup code

--- infrastructure/bootstrap/outputs.tf
+++ infrastructure/bootstrap/outputs.tf
@@ -7,3 +7,7 @@ output "dynamodb_table_name" {
   description = "Name of the DynamoDB table used for Terraform state locking"
   value       = aws_dynamodb_table.terraform_locks.name
 }
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
