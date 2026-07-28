4cd675e47b1e522ec62cc21946c506d077f20f85
kalandar chilakapati <kchilak@kalandars-MBP.lan>
2026-07-28 16:59:29 +0530

    updated files

--- infrastructure/environments/dev/locals.tf
+++ infrastructure/environments/dev/locals.tf
@@ -6,4 +6,7 @@ locals {
     project_name = var.project_name
     environment  = var.environment
   }
+
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
 }
