8d768cf1c71849e943b1ff7478cdc7be9ed3ff5f
kalandar chilakapati <kchilak@kalandars-MacBook-Pro.local>
2026-07-31 16:51:44 +0530

    updated ebs controller

---
+++ infrastructure/modules/eks/ebs-csi.tf
@@ -0,0 +1,41 @@
+data "aws_iam_policy_document" "ebs_csi_assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type = "Federated"
+
+      identifiers = [
+        aws_iam_openid_connect_provider.eks.arn
+      ]
+    }
+
+    condition {
+      test = "StringEquals"
+
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+
+      values = [
+        "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "ebs_csi" {
+
+  name = "${var.project_name}-${var.environment}-ebs-csi-role"
+
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+
+  role = aws_iam_role.ebs_csi.name
+
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
