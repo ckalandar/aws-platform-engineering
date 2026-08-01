db266a640ef42b0bcca82fa79272f75803cc8162
kalandar chilakapati <kchilak@kalandars-MacBook-Pro.local>
2026-08-02 00:42:56 +0530

    fixed karpener policy

--- infrastructure/modules/karpenter/main.tf
+++ infrastructure/modules/karpenter/main.tf
@@ -56,30 +56,55 @@ resource "aws_iam_role" "karpenter_controller" {
 
 resource "aws_iam_policy" "karpenter_controller" {
 
-  name = "${var.cluster_name}-karpenter-controller"
+  name = "${var.project_name}-${var.environment}-karpenter-controller"
 
   policy = jsonencode({
-
     Version = "2012-10-17"
 
     Statement = [
 
       {
         Effect = "Allow"
-
         Action = [
+          "eks:DescribeCluster"
+        ]
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:Describe*",
           "ec2:CreateLaunchTemplate",
           "ec2:DeleteLaunchTemplate",
-          "ec2:CreateFleet",
           "ec2:RunInstances",
+          "ec2:CreateFleet",
           "ec2:TerminateInstances",
-          "ec2:Describe*",
-          "ec2:CreateTags",
-          "pricing:GetProducts",
-          "ssm:GetParameter",
-          "iam:PassRole"
+          "ec2:CreateTags"
         ]
+        Resource = "*"
+      },
 
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "pricing:GetProducts"
+        ]
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole",
+          "iam:GetInstanceProfile",
+          "iam:CreateInstanceProfile",
+          "iam:DeleteInstanceProfile",
+          "iam:AddRoleToInstanceProfile",
+          "iam:RemoveRoleFromInstanceProfile",
+          "iam:TagInstanceProfile"
+        ]
         Resource = "*"
       }
     ]
